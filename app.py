f59d4b3aca02792be629b4303cb711f74f98908e
Amanda Roque <man.mroque@gmail.com>
2025-04-29 17:40:07 -0300

    update procfile e app.py

--- app.py
+++ app.py
@@ -165,4 +165,6 @@ def roi():
     return render_template('roi.html', resultado=resultado)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
