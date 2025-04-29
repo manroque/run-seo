49c19c059daadeb1b44ee59d9609a6e6cb22fd6d
Amanda Roque <100795886+manroque@users.noreply.github.com>
2025-04-29 19:40:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -188,5 +188,5 @@ def ping():
 
 # === INICIALIZAÇÃO DO SERVIDOR (apenas para testes locais) ===
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
